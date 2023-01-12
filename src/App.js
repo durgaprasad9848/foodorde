3f30e3069d3fcedba8a7e687fa7bcbf01730adaa
durgaprasad9848 <pulipatiprasad2000@gmail.com>
2023-01-12 12:29:01 +0530

    foodlog

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import { Fragment } from "react";
+
+import React from 'react';
+function App() {
+ return (
+  
+  <Fragment>
+    <Header />
+    <main>
+      <Meals/>
+
+    </main>
+     </Fragment>
+ );
+}
+
+export default App;
+ 
